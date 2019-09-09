3f50a014990c2fef0c537c336c9d5edb36cef477
Pedde136 <pedram.khanzadenvakilkandi@elev.ga.ntig.se>
2019-09-09 13:09:20 +0200

    klar för dagen

---
+++ V.37/first/first.java
@@ -0,0 +1,26 @@
+package first;
+
+public class first {
+
+	public static void main(String[] args) {
+		
+
+
+		int heltal = 5; 
+		int svar;
+
+		svar = heltal*heltal;
+
+		System.out.print("Svar:" + heltal*heltal);
+
+		double pi = 3.14;
+		double radie = 5;
+		double area = pi*radie*radie;
+
+		System.out.print(" Svar: " + area +"cm^2");
+
+	}
+}
+
+
+ 
---
+++ V.37/first/provasjälv.java
@@ -0,0 +1,26 @@
+package first;
+
+public class provasjälv {
+
+public static void main(String[] args) {
+		
+	
+		int heltal = 15; 
+		int svar;
+
+		svar = heltal*heltal;
+
+		System.out.print("Svar:" + heltal*heltal);
+
+		double pi = 3.14;
+		double radie = 15;
+		double area = pi*radie*radie;
+
+		System.out.print(" Svar: " + area +"cm^2");
+
+		
+
+
+
+}
+}

435c53bb697a82c6288aa30ad89e7fa9f3442db0
Pedde136 <pedram.khanzadenvakilkandi@elev.ga.ntig.se>
2019-09-10 12:06:55 +0200

    Lektion 2 String

---
+++ V.37/first/lektion2.java
@@ -0,0 +1,47 @@
+package first;
+
+import java.util.Scanner;
+
+public class lektion2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+Scanner	input = new Scanner (System.in);
+
+String namn1 = "PeePeePooPoo"; 
+String namn2 = "Sven";
+String namn3 = "Joergen";
+
+System.out.println("Skriv ett namn");
+
+String namn4 = input.nextLine();
+
+System.out.println("Skriv ett nanm");
+
+String namn5 = input.nextLine();
+
+System.out.println("\nNAMN");
+System.out.println(namn1 + "\n" + namn2 + "\n" + namn3 + "\n" + namn4 + "\n" + namn5);
+
+
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+
+}

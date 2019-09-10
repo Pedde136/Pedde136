a901051a2bd6e47feb439c6408e4f155d2a3f248
Pedde136 <pedram.khanzadenvakilkandi@elev.ga.ntig.se>
2019-09-10 12:12:12 +0200

    laxavecka 37 commit (2)

--- V.37/first/laxavecka37.java
+++ V.37/first/laxavecka37.java
@@ -1,10 +1,31 @@
 package first;
 
+import java.util.Scanner;
+
 public class laxavecka37 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Scanner	input = new Scanner (System.in);	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	}
 
 }

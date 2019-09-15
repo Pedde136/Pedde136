4ea8a5bbd9cda97784f06a8c6a887f057e90ab58
Pedde136 <pedram.khanzadenvakilkandi@elev.ga.ntig.se>
2019-09-15 18:51:47 +0200

    Läxan är klar

--- V.37/first/laxavecka37.java
+++ V.37/first/laxavecka37.java
@@ -8,55 +8,40 @@ public class laxavecka37 {
 		// TODO Auto-generated method stub
 
 		Scanner	input = new Scanner (System.in);	
-	
+		
+		
 		System.out.println("Vad heter du?");
-	
 	    String namn1 = input.nextLine();
 	
 	   
 	    System.out.println("Hur gammal ‰r du?");
-	    
 	    String namn2 = input.nextLine();
 
 	    
 	    System.out.println("Vad ‰r din adress?");
-	    
 	    String namn3 = input.nextLine();
 	
 
-	    System.out.println("Vilken stad bor du i?");
-	    
+	    System.out.println("Vad ‰r ditt postnummer?");
 	    String namn4 = input.nextLine();
 	    
 	    
-	    System.out.println("Vad ‰r ditt postnummer?");
-	    
+	    System.out.println("Vilken stad bor du i?");
 	    String namn5 = input.nextLine();
+	    
+	   
 	    System.out.println("Vad ‰r ditt telefonnummer?");
 	    String namn6 = input.nextLine();
 	    
 	    
 	    
 	    
-	    
-	    System.out.println("\nNAMN:");
-	    System.out.println(namn1 + "\n");
+	   
+	    System.out.println("\nINFORMATION");    
+	    System.out.println("\nNAMN:\t" + namn1 +"\n≈LDER:\t\t" + namn2 + "\nADRESS:\t\t" + namn3 + "\n\t\t" + "\t" + namn5 + "\nTELEFON:\t" + namn6);
 
-	    System.out.println("\n≈lder:");
-	    System.out.println(namn2 + "\n");
-	    
-	    System.out.println("\nAdress/Bostad:");
-	    System.out.println(namn3 + "\n" + namn4);
-	    
-	    System.out.println("\nPostnummer:");
-	    System.out.println(namn5 + "\n");
-	    
-	    System.out.println("\nTelefonnummer:");
-	    System.out.println(namn6 + "\n");
-	    
-	    
-	    
-	    
+	
+	
 	}
 
 }

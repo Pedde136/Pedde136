c3b0bec91d547df21b910de269b0f2714b66e877
Pedde136 <pedram.khanzadenvakilkandi@elev.ga.ntig.se>
2019-09-15 16:52:23 +0200

    Klar läxa

--- V.37/first/laxavecka37.java
+++ V.37/first/laxavecka37.java
@@ -13,20 +13,50 @@ public class laxavecka37 {
 	
 	    String namn1 = input.nextLine();
 	
+	   
+	    System.out.println("Hur gammal ‰r du?");
+	    
+	    String namn2 = input.nextLine();
+
+	    
+	    System.out.println("Vad ‰r din adress?");
+	    
+	    String namn3 = input.nextLine();
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
+	    System.out.println("Vilken stad bor du i?");
+	    
+	    String namn4 = input.nextLine();
+	    
+	    
+	    System.out.println("Vad ‰r ditt postnummer?");
+	    
+	    String namn5 = input.nextLine();
+	    System.out.println("Vad ‰r ditt telefonnummer?");
+	    String namn6 = input.nextLine();
+	    
+	    
+	    
+	    
+	    
+	    System.out.println("\nNAMN:");
+	    System.out.println(namn1 + "\n");
+
+	    System.out.println("\n≈lder:");
+	    System.out.println(namn2 + "\n");
+	    
+	    System.out.println("\nAdress/Bostad:");
+	    System.out.println(namn3 + "\n" + namn4);
+	    
+	    System.out.println("\nPostnummer:");
+	    System.out.println(namn5 + "\n");
+	    
+	    System.out.println("\nTelefonnummer:");
+	    System.out.println(namn6 + "\n");
+	    
+	    
+	    
+	    
 	}
 
 }

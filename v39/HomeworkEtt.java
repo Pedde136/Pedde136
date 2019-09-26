d7c31e794c6dcc8d6e4dd22f6163bf9d2ca3132d
Pedde136 <pedram.khanzadenvakilkandi@elev.ga.ntig.se>
2019-09-26 13:15:34 +0200

    Läxa 1 V.39

---
+++ v39/HomeworkEtt.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class HomeworkEtt {
+
+	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);
+
+		int r1 = input.nextInt();
+		int s = input.nextInt();
+		System.out.println(s - (r1 - s));
+
+	}
+
+}
